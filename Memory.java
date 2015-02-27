884828876871efa2a9381557572f94f824539fe0
Hanna Andersson <hannaand@nc2504-26.nc2504.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2015-02-27 16:37:49 +0100

    Yet more updates...

--- Memory.java
+++ Memory.java
@@ -8,13 +8,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Memory extends JFrame implements ActionListener {
+	
     File folder;
     File[] pictures;
     Card[] allCards;
     Card[] cards;
-    int X, Y;
+    int X, Y, p;
     JPanel upp,ner,spelare,spelplan;
     JButton nytt,slut;
+    Spelare[] players;
     int player = 0;
     int fas = 0; 
     Card[] visibleCards = new Card[2];
@@ -40,21 +42,49 @@ public class Memory extends JFrame implements ActionListener {
         setLayout(new BorderLayout());
         
         ner.setLayout(new GridLayout(1,2));
-        spelare.setLayout(new GridLayout(2, 1));
         add(ner,BorderLayout.SOUTH);
         add(spelare,BorderLayout.WEST);
         add(spelplan,BorderLayout.CENTER);
         ner.add(nytt);      ner.add(slut);
         
+        nytt.addActionListener(this);
+        slut.addActionListener(this);
+        
         newGame();
     }
 
     public void newGame() {
-        X = Integer.parseInt(JOptionPane.showInputDialog("Hur många rader?"));
-        Y = Integer.parseInt(JOptionPane.showInputDialog("Hur många kolumner?"));
-        spelplan.removeAll();
-        Tools.randomOrder(allCards);
-        cards = new Card[X*Y];
+    	
+        p = Integer.parseInt(JOptionPane.showInputDialog("Hur många spelare?"));
+        players = new Spelare[p];
+        
+        for (int i = 0; i < p; i++) {
+        	players[i] = new Spelare(i+1);
+        }
+        players[0].toggleTurn();
+        player = 0;
+    	fas = 0;
+        
+    	spelare.removeAll();
+    	spelare.setLayout(new GridLayout(p, 1));
+    	for (int i = 0; i < p; i++) {
+        	spelare.add(players[i]);
+        }
+    	
+    	do {
+    		X = Integer.parseInt(JOptionPane.showInputDialog("Hur många rader?"));
+            Y = Integer.parseInt(JOptionPane.showInputDialog("Hur många kolumner?"));
+            spelplan.removeAll();
+            Tools.randomOrder(allCards);
+            cards = new Card[X*Y];
+            if((X*Y % 2) != 0 ){
+            	JOptionPane.showMessageDialog(null, "Måste vara ett jämnt antal kort, idiot!");
+            }
+    	} while (X*Y%2 != 0);
+    	
+    	
+        
+        
         for (int i = 0; i < X*Y/2; i++) {
             cards[i] = allCards[i].copy();
             cards[i].addActionListener(this);
@@ -69,8 +99,6 @@ public class Memory extends JFrame implements ActionListener {
             cards[i].setActionCommand(Integer.toString(i));
             spelplan.add(cards[i]);
         }
-        nytt.addActionListener(this);
-        slut.addActionListener(this);
         pack();
         setVisible(true);
     }
@@ -91,25 +119,44 @@ public class Memory extends JFrame implements ActionListener {
             else {
                 visibleCards[0].setStatus(Card.Status.HIDDEN);
                 visibleCards[1].setStatus(Card.Status.HIDDEN);
+                players[player].toggleTurn();
+                if (player < p-1) {
+                	player++;
+                }
+                else {
+                	player = 0;
+                }
+                players[player].toggleTurn();
             }
             fas = 0;
-            if (player == 0) {player = 1;} else if (player == 1) {player = 0;}
             timer.stop();
+            int x = 0;
+            for (int i = 0; i < p; i++) {
+            	x += players[i].getPoints();
+            }
+            if (x == X*Y/2) {
+            	if(JOptionPane.showConfirmDialog(null,
+            			"Vill du spela igen?", "", JOptionPane.YES_NO_OPTION) == 0) {
+            		newGame();
+            	}
+            }
         }
         else {
             String iString = e.getActionCommand();
             int i = Integer.parseInt(iString);
-            if (fas == 0) {
+            if (fas == 0 && cards[i].getStatus() != Card.Status.MISSING) {
                 cards[i].setStatus(Card.Status.VISIBLE); 
                 visibleCards[0] = cards[i];
                 fas = 1;
             }
-            else if (fas == 1) {
+            else if (fas == 1 && cards[i].getStatus() != Card.Status.MISSING) {
                 cards[i].setStatus(Card.Status.VISIBLE);
                 visibleCards[1] = cards[i];
                 fas = 2;
+                if (visibleCards[0].equalIcon(visibleCards[1])) {
+                	players[player].addPoint();
+                }
                 timer.start();
-                
             }
         }
         
---
+++ Spelare.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Spelare extends JPanel {
+	
+	int poäng = 0;
+	int nr;
+	boolean tur = false;
+	
+	public Spelare (int i){
+		nr = i;
+		setBackground(Color.GRAY);
+		setLayout(new GridLayout(2, 1));
+		update();
+	}
+	
+	public void update() {
+		removeAll();
+		add(new JLabel("Spelare " + nr));
+		add(new JLabel(Integer.toString(poäng)));
+	}
+	
+	public void addPoint() {
+		poäng++;
+		update();
+	}
+	
+	public int getPoints() {
+		return poäng;
+	}
+	
+	public void toggleTurn() {
+		if (tur) {
+			setBackground(Color.GRAY);
+			tur = false;
+		}
+		else {
+			setBackground(Color.YELLOW);
+			tur = true;
+		}
+	}
+}
